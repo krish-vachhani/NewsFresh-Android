1786ac3dfa03a3db06ca6bbfce3d6fb01b4c6910
Krish Vachhani <krishvachhani19@gmail.com>
2021-10-02 09:33:18 +0530

    JSON object created and all the items are parsed but only the title is shown in recycler view

--- app/src/main/java/com/example/newsfresh/MainActivity.kt
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -1,30 +1,65 @@
 package com.example.newsfresh
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
+    private lateinit var mAdapter: NewsListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        val items = fetchData()
-        val adapter = NewsListAdapter(items,this)
-        recyclerView.adapter = adapter
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
     }
-    private fun fetchData(): ArrayList<String> {
-        val list = ArrayList<String>()
-        for(i in 0 until 100){
-            list.add("Item $i")
+    private fun fetchData() {
+
+        val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=f7d5618b6b5e495cb2dd9e088618108e"
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
         }
-        return list
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this,"clicked item is $item",Toast.LENGTH_LONG).show()
+    override fun onItemClicked(item: News) {
+
     }
 }
--- app/src/main/java/com/example/newsfresh/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsfresh/NewsListAdapter.kt
@@ -6,7 +6,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NewsListAdapter(private val items:ArrayList<String>,private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+class NewsListAdapter(private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+
+    private val items:ArrayList<News> = ArrayList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
         val viewHolder = NewsViewHolder(view)
@@ -18,17 +21,22 @@ class NewsListAdapter(private val items:ArrayList<String>,private val listener:N
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val currentItem = items[position]
-        holder.titleView.text = currentItem
+        holder.titleView.text = currentItem.title
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
+    fun updateNews(updatedNews:ArrayList<News>){
+        items.clear()
+        items.addAll(updatedNews)
 
+        notifyDataSetChanged()
+    }
 }
 class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val titleView = itemView.findViewById<TextView>(R.id.title)
+    val titleView: TextView = itemView.findViewById(R.id.title)
 }
 interface NewsItemClicked{
-    fun onItemClicked(item:String)
+    fun onItemClicked(item:News)
 }
